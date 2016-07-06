e9263290b455584cda9db88434cc6915ff4381d8
antisocial <willburninhell@gmail.com>
2016-07-06 19:27:39 +0300

    new frontpage

--- js/common.js
+++ js/common.js
@@ -8,107 +8,145 @@ $(function() {
 	};
 
 	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
+
+	$("a").smoothScroll();
 	
 });
 
-$(".scroll-top").click(function() {
-  $("html, body").animate({ scrollTop: 0 }, "slow");
-});
-
 $(window).load(function() {
-
 	$(".loader_inner").fadeOut();
 	$(".loader").delay(400).fadeOut("slow");
-	$("body").vegas({
-		timer:false,
-		transitionDuration:2000,
-
-		slides: [
-		{ src: "./img/bg_01.jpg" },
-		{ src: "./img/bg_02.jpg" },
-		{ src: "./img/bg_03.jpg" },
-		{ src: "./img/bg_04.jpg" }
-		]
-	});
+	$(".player").YTPlayer();
+	if ($(window).scrollTop() > 400) {
+		$(".nav").css("display", "inline-block");
+	}
 });
 
-$(document).ready(function(){
+$("#greeting").typed({
+	loop: false,
+	strings: ["a web-developer.", "a freelancer.", "Antik."],
+	typeSpeed: 25,
+	backSpeed: 10,
+	startDelay: 500,
+	backDelay: 500,
+	callback: function() {
+		$('.second-line').fadeIn("slow",function(){$('.link-line').fadeIn("slow");});
+	}
+});
 
-	$('.gallery-popup').magnificPopup({
+$(window).on("scroll", function () {
+	if ($(window).scrollTop() > 400) {
+		$(".nav").css("display", "inline-block");
+	}
+});
+
+$(".link-line a").on("click", function(){
+	$("body").css("overflow-y", "visible");
+});
+
+$('.prof-skills').highcharts({
+	chart: {
+		type: 'bar',
+		backgroundColor: 'transparent',
+	},
+	tooltip: {
+		enabled: false
+	},
+	title: {
+		text: 'Professional skills',
+		style: {"color": "#f0f0f0", "fontSize": "24px"},
+	},
+	xAxis: {
+		categories: ['HTML', 'CSS', 'jQuery', 'Django', 'Bootstrap' ,'Photoshop', 'Linux', 'SQL', 'Git'],
+		labels: {
+			style: {"color": "#f0f0f0", "shadow": "none", "fontSize": "18px"},
+		},
+		gridLineColor: 'transparent',
+	},
+	yAxis: {
+		min: 0,
+		labels: {enabled: false},
+		title: {enabled: false},
+		gridLineColor: 'transparent',
+	},
+	credits: {
+		enabled: false
+	},
+	series: [{
+		name: '',
+		showInLegend: false,
+		data: [90, 80, 70, 50, 100, 50, 80, 70, 80],
+	}]
+});
+
+$('.pers-skills').highcharts({
+	chart: {
+		type: 'bar',
+		backgroundColor: 'transparent',
+	},
+	tooltip: {
+		enabled: false
+	},
+	title: {
+		text: 'Personal skills',
+		style: {"color": "#f0f0f0", "fontSize": "24px"},
+	},
+	xAxis: {
+		categories: ['Attentive', 'Accurate', 'Cooperative', 'Dedicated', 'Efficient', 'Flexible', 'Motivated', 'Reliable', 'Responsible'],
+		labels: {
+			style: {"color": "#f0f0f0", "shadow": "none", "fontSize": "18px"},
+		},
+		gridLineColor: 'transparent',
+		opposite: true,
+	},
+	yAxis: {
+		max: 0,
+		labels: {enabled: false},
+		title: {enabled: false},
+		gridLineColor: 'transparent',
+	},
+	credits: {
+		enabled: false
+	},
+	series: [{
+		name: '',
+		showInLegend: false,
+		data: [-80, -90, -75, -100, -100, -80, -80, -90, -80],
+	}]
+});
+
+$(".portfolio-carousel").owlCarousel({
+	loop: true,
+	center: false,
+	dots: true,
+	autoplay: true,
+	autoplayHoverPause:true,
+	responsive : {
+    // breakpoint from 0 up
+    0 : {
+    	items: 1
+    },
+    480 : {
+    	items: 2
+    },
+    768 : {
+    	items: 3
+    },
+    // breakpoint from 768 up
+    991 : {
+    	items: 3
+    }
+  }
+});
+
+$('[data-toggle="tooltip"]').tooltip();
+
+
+	$('.portfolio-popup').magnificPopup({
 		type:'image',
 		gallery:{
 			enabled:true
 		}
 	});
 
-	$(".top-owl-carousel").owlCarousel({
-		loop: true,
-		items: 1,
-		center: true,
-		dots: true,
-		autoplay: true
-	});
 
-	$(".news-owl-carousel").owlCarousel({
-		loop: true,
-		center: false,
-		dots: true,
-		autoplay: true,
-		responsive : {
-    // breakpoint from 0 up
-    0 : {
-        items: 1
-    },
-    480 : {
-        items: 2
-    },
-    768 : {
-        items: 3
-    },
-    // breakpoint from 768 up
-    991 : {
-        items: 4
-    }
-}
-	});
-
-	$(".testimonials-carousel").owlCarousel({
-		loop: true,
-		items: 1,
-		center: true,
-		dots: true,
-		autoplay: true
-	});
-
-	var wow = new WOW(
-	{
-    boxClass:     'wow',      // animated element css class (default is wow)
-    animateClass: 'animated', // animation css class (default is animated)
-    offset:       50,          // distance to the element when triggering the animation (default is 0)
-    mobile:       true,       // trigger animations on mobile devices (default is true)
-    live:         true,       // act on asynchronously loaded content (default is true)
-    callback:     function(box) {
-      // the callback is fired every time an animation is started
-      // the argument that is passed in is the DOM node being animated
-    },
-    scrollContainer: null // optional scroll container selector, otherwise use window
-  }
-  );
-	wow.init();
-	$('a').smoothScroll();
-
-	$(function(){
-    $('#filter-container').mixItUp();
-	});
-});
-
-$(window).on("scroll", function () {
-            if ($(window).scrollTop() > 200) {
-                $(".navbar").removeClass("transparent-nav");
-                $(".scroll-top").css("display", "block");
-            }
-            else {
-                $(".navbar").addClass("transparent-nav");
-                $(".scroll-top").css("display", "none");
-            }
-        });
